3da158f1e577ebddc69e1e0821f1f7d79fe5d66d
Alexandre Lewkowicz <lewkow_a@epita.fr>
2014-10-28 17:30:22 +0100

    Program: add binding methods

--- src/program.cpp
+++ src/program.cpp
@@ -2,6 +2,16 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
+program::program(texture_manager_ptr tm)
+    : texture_manager_(tm)
+{
+}
+
+auto program::get_texture_manager() -> const texture_manager_ptr& 
+{
+    return texture_manager_;
+}
+
 template <typename T>
 void
 program::load_data(GLuint program_id, std::vector<T> &data, const char *name,
@@ -29,3 +39,48 @@ program::load_material(material& mat)
     glUniform4fv(material_location_.ambient, 1, value_ptr(mat.get_ambient()));
     glUniform1f(material_location_.shininess, mat.get_shininess());
 }
+
+template <typename T>
+void
+program::load_vertex_buffer(std::vector<T>& vertices, GLuint* vert_buffer_id)
+{
+    glGenBuffers(1, vert_buffer_id);
+    glBindBuffer(GL_ARRAY_BUFFER, *vert_buffer_id);
+    long unsigned byte_size = sizeof (T) * vertices.size();
+    glBufferData(GL_ARRAY_BUFFER, byte_size, vertices.data(), GL_STATIC_DRAW);
+
+}
+
+void
+program::set_vao_attribs(bool has_text_coord, size_t stride)
+{
+    // Sets shaders attribute for vertices positions
+    GLint pos_location = glGetAttribLocation(program_id_, "in_position");
+    glEnableVertexAttribArray(pos_location); // Matches layout (location = 0)
+    glVertexAttribPointer(pos_location, 3, GL_FLOAT, GL_FALSE, stride, 0);
+
+    // Sets shaders attribute for texture coordinates
+    if (has_text_coord)
+    {
+        GLint uv_idx = glGetAttribLocation(program_id_, "in_uv");
+        if (uv_idx != -1) {
+            glEnableVertexAttribArray(uv_idx); // Matches layout (location = 1)
+            GLvoid *offset = reinterpret_cast<GLvoid *> (sizeof (glm::vec3));
+            glVertexAttribPointer(uv_idx, 2, GL_FLOAT, GL_FALSE, stride,
+                                  offset);
+        }
+    }
+    // Sets shaders attribute for color (r, g, b)
+    GLint norm_idx = glGetAttribLocation(program_id_, "in_norm");
+    if (norm_idx != -1) {
+        glEnableVertexAttribArray(norm_idx); // Matches layout (location = 1)
+        GLvoid *offset;
+        if (has_text_coord)
+            offset = reinterpret_cast<GLvoid *> (sizeof (glm::vec3) +
+                                                 sizeof (glm::vec2));
+        else
+            offset = reinterpret_cast<GLvoid *> (sizeof (glm::vec3));
+        glVertexAttribPointer(norm_idx, 3, GL_FLOAT, GL_FALSE, stride, offset);
+    }
+}
+
--- src/program.hpp
+++ src/program.hpp
@@ -6,16 +6,24 @@
 # include <memory>
 
 # include "texture_manager.hpp"
+# include "utility.hpp"
 
 class program
 {
 public:
+    using texture_manager_ptr = std::shared_ptr<texture_manager>;
+    using container_vtn = std::vector<utility::vertex_vtn>;
+    using container_vn = std::vector<utility::vertex_vn>;
     template <typename T>
     void load_data(GLuint program_id, std::vector<T>& data, const char* name,
                    GLuint* buffer_id);
     void load_material(material& mat);
+    program(texture_manager_ptr tm);
+    const texture_manager_ptr& get_texture_manager();
+    template <typename T>
+    void load_vertex_buffer(std::vector<T>& vertices, GLuint* vert_buffer_id);
+    void set_vao_attribs(bool has_text_coord, size_t stride);
 private:
-    using texture_manager_ptr = std::shared_ptr<texture_manager>;
     texture_manager_ptr texture_manager_;
     GLuint program_id_;
     struct material_location
